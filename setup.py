a9c8b70ac1b4df581bd12e72f2bb44c207d66b56
François Leblanc <francois.leblanc@shopify.com>
2023-06-25 22:24:23 -0400

    Init
    Still need to paste initial LLM dump, including stubbed code and tests.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="paca",
+    version="0.0.1",
+    description="Local large language model service",
+    author="François Leblanc",
+    author_email="leblancfg@gmail.com",
+    packages=["paca"],
+    python_requires=">=3.9",
+)
